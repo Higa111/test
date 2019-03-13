e9d457ea60c055aa67a441d76a35fe217baa0270
hirakata09231025 <tinolwm.f@docomo.ne.jp>
2019-03-13 21:49:38 +0900

    2019/03/13 Halloworld java#3

---
+++ testproject01/src/testproject01/Halloworld.java
@@ -0,0 +1,20 @@
+package testproject01;
+
+public class Halloworld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hallo world!!");
+		//Hallo world!!を出力します
+		//間違いやすい所：Systemの頭文字は大文字！
+
+		System.out.println(123+12);
+		//ダブルクォーテーションで囲まない数値は四則演算等で出力できる
+
+		System.out.println("Hallo"+"World");
+		//文字列同士をくっつける
+		System.out.println("例外が発生"
+				+"しました。");
+	}
+
+}
